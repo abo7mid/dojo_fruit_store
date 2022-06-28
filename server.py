e1bd84095d6a34bc7710e86b5445cfd66694ebe5
Ahmed Nadawy <a.anadawy@gmail.com>
2022-06-28 10:00:57 +0300

    Dojo Fruit Store

--- server.py
+++ server.py
@@ -5,10 +5,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+
+    count = int((request.form['strawberry']))+int(request.form['raspberry'])+int(request.form['apple'])
+    print(f"Charging {request.form['first_name']} for {count} fruits")
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    apple = request.form['apple']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    return render_template("checkout.html",strawberry=strawberry,raspberry=raspberry,apple=apple,first_name=first_name,last_name=last_name,student_id=student_id)
+
+
+
 
 @app.route('/fruits')         
 def fruits():
